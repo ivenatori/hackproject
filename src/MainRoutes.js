aebca9bd8e07fd0e273a2b967bdcd123551c8364
Чингиз Ниязалиев <cingizniazaliev@MacBook-Air-Cingiz.local>
2021-11-12 18:02:12 +0600

    header

--- src/MainRoutes.js
+++ src/MainRoutes.js
@@ -1,18 +1,21 @@
 import React from "react";
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PrimarySearchAppBar from "./components/Header/Header";
+
 import ProductsList from "./components/ProductsList/ProductsList";
 const MainRoutes = () => {
   return (
-    <AuthContextProvider>
-      <ProductsContextProvider>
+    // <AuthContextProvider>
+    //   <ProductsContextProvider>
         <BrowserRouter>
-          <Header />
+          <PrimarySearchAppBar/>
           <Routes>
             <Route path="/list" element={<ProductsList />} />
           </Routes>
         </BrowserRouter>
-      </ProductsContextProvider>
-    </AuthContextProvider>
+    //   </ProductsContextProvider>
+    // </AuthContextProvider>
   );
 };
 
