ec7bc56fe1115e3d2568b315d35d4e3870d5ddb1
Erlan <erachynybaev@gmail.com>
2021-11-12 21:48:49 +0600

    add product

---
+++ src/components/AddProduct/AddProduct.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const AddProduct = () => {
+    return (
+        <div className='add_product container'>
+            <div><input type="text" />name</div>
+            <div><input type="text" />image</div>
+            <div><input type="text" />about</div>
+            <div><input type="text" />price small</div>
+            <div><input type="text" />price large</div>
+            <div><input type="text" />price small</div>  
+        </div>
+    );
+};
+
+export default AddProduct;
