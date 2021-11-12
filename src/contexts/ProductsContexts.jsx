096313ecf24cbbe2c254afbd17b2e642ff9550e5
Чингиз Ниязалиев <cingizniazaliev@MacBook-Air-Cingiz.local>
2021-11-12 16:19:25 +0600

    new context and components edit

--- src/contexts/ProductsContexts.jsx
+++ src/contexts/ProductsContexts.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Header = () => {
+const ProductsContexts = () => {
     return (
         <div>
             
@@ -8,4 +8,4 @@ const Header = () => {
     );
 };
 
-export default Header;
+export default ProductsContexts;
