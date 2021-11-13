99630715f63577b7b27dc8e8cee806c01ed04e06
Erlan <erachynybaev@gmail.com>
2021-11-13 18:20:04 +0600

    filter

--- src/components/AddProduct/AddProduct.jsx
+++ src/components/AddProduct/AddProduct.jsx
@@ -14,6 +14,16 @@ const AddProduct = () => {
         <div className='bg_add_product'>
         <div className='container'>
         <div className='add_product '>
+            <div><input onChange={(e)=>setProduct({...product,product: e.target.value})} list='data' type="text" />
+            <datalist id='data'>
+                <option value="pizza"></option>
+                <option value="sushi"></option>
+                <option value="drinks"></option>
+                <option value="sous"></option>
+                <option value=""></option>
+                <option value=""></option>
+            </datalist>
+            </div>
             <div><input onChange={(e)=>setProduct({...product,Pname: e.target.value})} type="text" /><label>pizza name</label></div>
             <div><input onChange={(e)=>setProduct({...product,image: e.target.value})} type="text" /><label>image</label></div>
             <div><input onChange={(e)=>setProduct({...product,about: e.target.value})} type="text" /><label>about</label></div>
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { productsContext } from '../../contexts/ProductsContext';
+import history from '../../helpers/history';
 import './Filter.css'
 
 
 const Filter = () => {
+    const {getProducts} = useContext(productsContext)
+
+    function handleFilter(e) {
+        console.log(e)
+        const search = new URLSearchParams(history.location.search)
+        search.set('product', e)
+        history.push(`${history.location.pathname}?${search.toString()}`)
+ 
+        getProducts(search.toString())
+      }
     return (
         <div className='filter'>
             <div className='container'>
                     <div className='filter_menu'>
                         <ul className='filter_menu-flex'>
-                            <li>пиццы</li>
-                            <li>роллы</li>
-                            <li>Мексиканская</li>
-                            <li>4 сезона</li>
-                            <li>напитки</li>
-                            <li>соусы</li>
+                            <li onClick={(e)=>handleFilter('pizza')}>пиццы</li>
+                            <li onClick={(e)=>handleFilter('sushi')}>роллы</li>
+                            <li onClick={(e)=>handleFilter('drinks')}>напитки</li>
+                            <li onClick={(e)=>handleFilter('sous')}>4 соусы</li>
+                            <li onClick={(e)=>handleFilter()}>ntcn</li>
+                            <li onClick={(e)=>handleFilter()}>ntct</li>
                         </ul>
                     </div>
             </div>
